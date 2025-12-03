832e68f636a15867a5ba1ceb6e82aa7e6aedec5d
domus1979 <domus1979@mail.ru>
2025-12-03 20:18:40 +0300

    Change reader

--- src/main/java/by/dvn/firsttask/readfromfile/ReadCustomArray.java
+++ src/main/java/by/dvn/firsttask/readfromfile/ReadCustomArray.java
@@ -1,7 +1,9 @@
 package by.dvn.firsttask.readfromfile;
 
+import by.dvn.firsttask.exception.CustomArrayException;
+
 import java.util.List;
 
 public interface ReadCustomArray {
-    List<String> readFileToList(String fileName);
+    List<String> readFileToList(String fileName) throws CustomArrayException;
 }
--- src/main/java/by/dvn/firsttask/readfromfile/impl/ReadCustomArrayImpl.java
+++ src/main/java/by/dvn/firsttask/readfromfile/impl/ReadCustomArrayImpl.java
@@ -1,12 +1,14 @@
 package by.dvn.firsttask.readfromfile.impl;
 
 import by.dvn.firsttask.entity.CustomIntArray;
+import by.dvn.firsttask.exception.CustomArrayException;
 import by.dvn.firsttask.readfromfile.ReadCustomArray;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,13 +18,20 @@ public class ReadCustomArrayImpl implements ReadCustomArray {
     private static final String DIRECTORY = "data/";
 
     @Override
-    public List<String> readFileToList(String fileName) {
+    public List<String> readFileToList(String fileName) throws CustomArrayException {
         List<String> lines = new ArrayList<>();
 
+        if (fileName.strip().length() == 0) {
+            throw new CustomArrayException("File name is empty.");
+        }
+        if (Files.notExists(Path.of(DIRECTORY + fileName))) {
+            throw new CustomArrayException("File not exist.");
+        }
+
         try {
-           lines = Files.readAllLines(Paths.get(DIRECTORY + fileName));
+            lines = Files.readAllLines(Paths.get(DIRECTORY + fileName));
         } catch (IOException ex) {
-            log.error(ex.getMessage());
+            throw new CustomArrayException(ex);
         }
 
         log.info("Read array file.");
