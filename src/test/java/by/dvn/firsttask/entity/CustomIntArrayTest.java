fc2cf46fc78c7835b95c9f13b5bb3a035a074b6f
domus1979 <domus1979@mail.ru>
2025-12-03 20:01:50 +0300

    Add some test.

---
+++ src/test/java/by/dvn/firsttask/entity/CustomIntArrayTest.java
@@ -0,0 +1,66 @@
+package by.dvn.firsttask.entity;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomIntArrayTest {
+
+    private CustomIntArray customArray;
+
+    @BeforeEach
+    public void setUp() {
+        customArray = null;
+    }
+
+    @Test
+    public void testDefaultConstructor() {
+        customArray = new CustomIntArray();
+        assertNull(customArray.getArr(), "Default constructor should create null array");
+    }
+
+    @Test
+    public void testConstructorWithSize() {
+        int size = 5;
+        customArray = new CustomIntArray(size);
+
+        assertNotNull(customArray.getArr(), "Array should not be null");
+        assertEquals(size, customArray.getArr().length, "Array length should match size parameter");
+
+        // Verify all elements are initialized to 0
+        for (int i = 0; i < size; i++) {
+            assertEquals(0, customArray.getArr()[i], "Elements should be initialized to 0");
+        }
+    }
+
+    @Test
+    public void testConstructorWithArray() {
+        int[] originalArray = {1, 2, 3, 4, 5};
+        customArray = new CustomIntArray(originalArray);
+
+        assertNotNull(customArray.getArr(), "Array should not be null");
+        assertArrayEquals(originalArray, customArray.getArr(), "Arrays should be equal");
+        assertNotSame(originalArray, customArray.getArr(), "Array should be a copy, not the same reference");
+    }
+
+    @Test
+    void getArr() {
+        int[] testArray = {10, 20, 30};
+        customArray = new CustomIntArray(testArray);
+
+        int[] result = customArray.getArr();
+        assertArrayEquals(testArray, result, "getArr should return array with same values");
+    }
+
+    @Test
+    void setArr() {
+        customArray = new CustomIntArray(3);
+        int[] newArray = {5, 10, 15};
+
+        customArray.setArr(newArray);
+
+        assertArrayEquals(newArray, customArray.getArr(), "setArr should update the array");
+        assertNotSame(newArray, customArray.getArr(), "setArr should create a copy");
+    }
+}
